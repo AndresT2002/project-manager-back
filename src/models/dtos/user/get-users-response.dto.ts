690f22388ce22fc0d68b3f499240de44602f496a
Andres Guillermo Toloza Guzman <73532719+AndresT2002@users.noreply.github.com>
2025-08-12 19:31:37 -0500

    Feat: Added User/Task get dtos

---
+++ src/models/dtos/user/get-users-response.dto.ts
@@ -0,0 +1,98 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  IsUUID,
+  IsDateString,
+  IsBoolean,
+} from 'class-validator';
+import { Role } from 'src/models/enums';
+import { PaginatedResponseDto } from '../common/pagination.dto';
+
+export class GetUserResponseDto {
+  @ApiProperty({
+    description: 'The ID of the user',
+    example: '123e4567-e89b-12d3-a456-426614174000',
+  })
+  @IsUUID()
+  @IsNotEmpty()
+  readonly id: string;
+
+  @ApiProperty({
+    description: 'The name of the user',
+    example: 'John',
+  })
+  @IsString()
+  @IsNotEmpty()
+  readonly name: string;
+
+  @ApiProperty({
+    description: 'The last name of the user',
+    example: 'Doe',
+  })
+  @IsString()
+  @IsNotEmpty()
+  readonly lastName: string;
+
+  @ApiProperty({
+    description: 'The full name of the user',
+    example: 'John Doe',
+  })
+  @IsString()
+  @IsNotEmpty()
+  readonly fullName: string;
+
+  @ApiProperty({
+    description: 'The email of the user',
+    example: 'john.doe@example.com',
+  })
+  @IsEmail()
+  @IsNotEmpty()
+  readonly email: string;
+
+  @ApiProperty({
+    description: 'The role of the user',
+    example: Role.MEMBER,
+    enum: Role,
+  })
+  @IsEnum(Role)
+  @IsNotEmpty()
+  readonly role: Role;
+
+  @ApiProperty({
+    description: 'The ID of the user who created this user',
+    example: '123e4567-e89b-12d3-a456-426614174000',
+  })
+  @IsUUID()
+  @IsNotEmpty()
+  readonly createdBy: string;
+
+  @ApiProperty({
+    description: 'The is active of the user',
+    example: true,
+  })
+  @IsBoolean()
+  @IsNotEmpty()
+  readonly isActive: boolean;
+
+  @ApiProperty({
+    description: 'When the user was created',
+    example: '2024-01-15T10:30:00.000Z',
+  })
+  @IsDateString()
+  @IsNotEmpty()
+  readonly createdAt: string;
+
+  @ApiProperty({
+    description: 'When the user was last updated',
+    example: '2024-01-15T10:30:00.000Z',
+  })
+  @IsDateString()
+  @IsNotEmpty()
+  readonly updatedAt: string;
+}
+
+// Usar el DTO genérico de paginación
+export type GetUsersResponseDto = PaginatedResponseDto<GetUserResponseDto>;
---
+++ src/models/dtos/user/index.ts
@@ -0,0 +1,14 @@
+// Request DTOs
+export { CreateUserDto } from './create-user-request.dto';
+export { UpdateUserDto } from './update-user-request.dto';
+
+// Response DTOs
+export { CreateUserResponseDto } from './create-user-response.dto';
+export { UpdateUserResponseDto } from './update-user-response.dto';
+export {
+  GetUserResponseDto,
+  GetUsersResponseDto,
+} from './get-users-response.dto';
+
+// Query DTOs
+export { GetUsersQueryDto } from './get-users-query.dto';
